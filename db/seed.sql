b8f9412de66cfc9aef6bfd507c6b91bb1f01caa2
Jacob Hoffman <cubhoffman@gmail.com>
2024-02-02 08:28:23 -0500

    ADD: initial db

---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    users (id, name, email)
+VALUES (
+        '1', 'Gopher', 'hello@gopher.com'
+    );
+
+INSERT INTO
+    users (id, name, email)
+VALUES (
+        '2', 'Ricky', 'hello@ricky.com'
+    );
+
+INSERT INTO
+    users (id, name, email)
+VALUES ('3', 'Zig', 'hello@zig.com');
+
+INSERT INTO
+    users (id, name, email)
+VALUES ('4', 'Ralfie', 'hello@ralfie.com');
---
+++ db/table/user.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS users (
+    "id" TEXT NOT NULL, "name" TEXT NOT NULL, "email" TEXT NOT NULL, PRIMARY KEY ("id")
+);
