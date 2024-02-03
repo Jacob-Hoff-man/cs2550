f2df1f8987834721e93a745e69e7a685498dc7a1
Jacob Hoffman <cubhoffman@gmail.com>
2024-02-03 09:06:17 -0500

    update sale schema based on hw2 assumptions

--- db/table/sale.sql
+++ db/table/sale.sql
@@ -4,8 +4,13 @@ CREATE TABLE
   sale (
     id serial NOT NULL,
     customer_id int NOT NULL,
+    coffee_id int NOT NULL,
+    store_id int NOT NULL,
     purchased_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    quantity int,
     balance float,
     CONSTRAINT pk_sale PRIMARY KEY (id),
-    CONSTRAINT fk_sale FOREIGN KEY (customer_id) REFERENCES customer (id) ON UPDATE CASCADE ON DELETE CASCADE
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_coffee FOREIGN KEY (coffee_id) REFERENCES coffee (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_store FOREIGN KEY (store_id) REFERENCES store (id) ON UPDATE CASCADE ON DELETE CASCADE
   );
