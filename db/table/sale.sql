d21f6e9c2ee32c00ed89dbeade0cc8407aca9326
Jacob Hoffman <cubhoffman@gmail.com>
2024-02-03 07:00:56 -0500

    ADD: sale table

---
+++ db/table/sale.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS sale CASCADE;
+
+CREATE TABLE
+  sale (
+    id serial NOT NULL,
+    customer_id int NOT NULL,
+    purchased_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    balance float,
+    CONSTRAINT pk PRIMARY KEY (id),
+    CONSTRAINT fk FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON UPDATE CASCADE ON DELETE CASCADE
+  );
