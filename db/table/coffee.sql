a7b7195dd7f783cace213a390a697e14bfad9990
Jacob Hoffman <cubhoffman@gmail.com>
2024-02-02 22:58:15 -0500

    ADD: coffee table

---
+++ db/table/coffee.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS coffee cascade;
+
+CREATE TABLE
+    coffee (
+        id serial NOT NULL,
+        name varchar(50) NOT NULL,
+        description VARCHAR(250),
+        country_of_origin varchar(60),
+        intensity int NOT NULL CHECK (
+            intensity >= 1
+            AND intensity <= 12
+        ),
+        price float NOT NULL CHECK (price >= 0),
+        CONSTRAINT pk PRIMARY KEY (id),
+    );
