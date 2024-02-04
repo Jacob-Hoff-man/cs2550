4172c5a77b7f002a85cb6523c501907c98ec0e05
Jacob Hoffman <cubhoffman@gmail.com>
2024-02-04 12:15:39 -0500

    ADD: HW2 Q3D Query

---
+++ db/query/hw2-q3d.sql
@@ -0,0 +1,23 @@
+-- Rank coffees based on the most quantity of the coffee that was sold by CoffeeDB.
+-- List: coffee id, coffee name, intensity, and rank.
+SELECT
+    coffee_id,
+    name,
+    intensity,
+    RANK() OVER (
+        ORDER BY
+            total_quantity DESC
+    ) coffee_sold_rank
+FROM
+    coffee C
+    NATURAL JOIN (
+        SELECT
+            coffee_id,
+            SUM(quantity) AS total_quantity
+        FROM
+            sale
+        GROUP BY
+            coffee_id
+    ) TQ
+WHERE
+    C.coffee_id = TQ.coffee_id
