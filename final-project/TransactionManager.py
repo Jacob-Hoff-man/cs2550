d771e9708676414db9272adf29388cda73ff3a98
Declan <dkutscher10@gmail.com>
2024-03-21 16:41:06 -0400

    changed the name

---
+++ final-project/TransactionManager.py
@@ -0,0 +1,19 @@
+import DeadlockDector
+
+
+##Round Robin (which reads one line from each file at a time in turns) 
+def round_robin():
+    
+    return
+
+##Random (which reads files in random order, and reads a random number of lines from each file).
+def random_read():
+
+    return
+
+def main():
+    dld = DeadlockDector()
+    
+    
+if "__name__" == "__main__":
+    main()
