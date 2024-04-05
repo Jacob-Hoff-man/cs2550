bcf5e380ecb9730a12c85467749bee359412bc26
Declan <dkutscher10@gmail.com>
2024-04-05 17:33:02 -0400

    added operation class and notes from office hours

--- final-project/TransactionManager.py
+++ final-project/TransactionManager.py
@@ -30,31 +30,59 @@ class Operation():
     def to_txn_tuple(self, line):
         elements = line.split(' ')
         match(elements[0]):
-            case OpType.B:
+            case OpType.B.value:
                 return (elements[0], elements[1])
-            case OpType.C:
-                return (elements[0])
-            case OpType.A:
-                return (elements[0])
-            case OpType.Q:
-                return (elements[0])
-            case OpType.I:
+            case OpType.C.value:
+                return (elements[0], None)
+            case OpType.A.value:
+                return (elements[0], None)
+            case OpType.Q.value:
+                return (elements[0], None)
+            case OpType.I.value:
                 return self.trim_operation_insert(line)
-            case OpType.U:
+            case OpType.U.value:
                 return self.trim_operation_insert(line)
-            case OpType.R:
+            case OpType.R.value:
                 return (elements[0], list(elements[1:]))
-            case OpType.T:
+            case OpType.T.value:
                 return (elements[0], elements[1])
-            case OpType.M:
+            case OpType.M.value:
                 return (elements[0], elements[1:])
-            case OpType.G:
+            case OpType.G.value:
                 return (elements[0], elements[1])
             case _:
+                print(elements[0])
                 quit("Op Type Not recognized...")
 
     def __init__(self, line) -> None:
         self.op, self.args = self.to_txn_tuple(line)
+
+    def __str__(self) -> str:
+        match(self.op):
+            case OpType.B.value:
+                return f"{self.op} {self.args}"
+            case OpType.C.value:
+                return f"{self.op}"
+            case OpType.A.value:
+                return f"{self.op}"
+            case OpType.Q.value:
+                return f"{self.op}"
+            case OpType.I.value:
+                tuple_str = f"({self.args[1][0]}, {self.args[1][1]}, {self.args[1][2]}, {self.args[1][3]})"
+                return f"{self.op} {self.args[0]} {tuple_str}"
+            case OpType.U.value:
+                return f"{self.op} {self.args[0]} ({self.args[1:]})"
+            case OpType.R.value:
+                return f"{self.op} {self.args[0]} {self.args[1]}"
+            case OpType.T.value:
+                return f"{self.op} {self.args[0]}"
+            case OpType.M.value:
+                return f"{self.op} {self.args[0]} {self.args[1]}"
+            case OpType.G.value:
+                return f"{self.op} {self.args[0]} {self.args[1]}"
+            case _:
+                return f"{self.op} {self.args}"
+        
 
 class Transaction():
     def __init__(self, id) -> None:
@@ -63,7 +91,11 @@ class Transaction():
     def add(self, op: Operation):
         self.ops.append(op)
     def get(self):
-        pass
+        return self.ops.pop(0)
+    def __len__(self):
+        return len(self.ops)
+    def pop(self, idx):
+        return self.ops.pop(idx)
 
 
 class TransactionManager():
@@ -75,10 +107,8 @@ class TransactionManager():
             for file_name in list(txns):
                 txn = txns[file_name]
                 if len(txn) > 0:
-                    op = txn.pop(0) # line   
-                    txn = Transaction(op)
-
-                    l.log(f"{os.path.splitext(os.path.split(file_name)[1])[0]}: {txn.op}{txn.args}")
+                    oper = txn.pop(0) # one operation   
+                    l.log(f"{os.path.splitext(os.path.split(file_name)[1])[0]}: {oper}")
                 else:
                     del txns[file_name]
     
@@ -91,10 +121,9 @@ class TransactionManager():
             file_name = file_names[rand_num]
             txn = txns[file_name]
             if len(txn) > 0:
-                op = txn.pop(0) # line   
-                txn = Transaction(op)
+                oper = txn.pop(0) # line   
 
-                l.log(f"{os.path.splitext(os.path.split(file_name)[1])[0]}: {txn.op}{txn.args}")
+                l.log(f"{os.path.splitext(os.path.split(file_name)[1])[0]}: {oper}")
             else:
                 del txns[file_name]
                 file_names.pop(rand_num)
@@ -102,12 +131,13 @@ class TransactionManager():
     def read_files(self, raw_file_names):
         txns = {}
         for file_name in raw_file_names:
-            txn = []
+            _id = 0
+            txn = Transaction(_id)
+
             with open(file_name) as f:
                 for line in f:
-                    txn.append(line.replace("\n", ""))
+                    txn.add(Operation(line.replace("\n", "")))
                     # make a new transaction
-            txns[file_name] = Operation(txn)
-            #txns[file_name] = txn
+            txns[file_name] = txn
 
         return txns
