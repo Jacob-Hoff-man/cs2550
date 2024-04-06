78ed26705348b9a47c5e9befd7f16f4fb1996d19
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-06 17:37:56 -0400

    ADD: ClusteredIndex class

---
+++ final-project/auxs/ClusteredIndex.py
@@ -0,0 +1,11 @@
+from Common import AuxiliaryStructure
+
+class ClusteredIndex(AuxiliaryStructure):
+    def set(self, key, page_key):
+        self.pages[key] = page_key
+
+    def get(self, key):
+        return self.pages.get(key)
+    
+    def __init__(self) -> None:
+        self.pages = {}
