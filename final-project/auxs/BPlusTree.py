5e2761908e30b5cb769afc558474039ac71ed929
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-06 17:38:26 -0400

    ADD: BPlusTree class using bplustree (WIP)

---
+++ final-project/auxs/BPlusTree.py
@@ -0,0 +1,12 @@
+from bplustree import BPlusTree as Bpt
+from Common import AuxiliaryStructure
+
+class BPlusTree(AuxiliaryStructure):
+    def set(self, key, page_key):
+        self.pages[key] = page_key
+
+    def get(self, key):
+        return self.pages.get(key)
+    
+    def __init__(self) -> None:
+        self.pages = Bpt('/tmp/bplustree.db', order=50)
