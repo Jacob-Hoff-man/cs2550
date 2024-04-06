9360c99ac2c6c5869a86335814135af915230bea
Declan <dkutscher10@gmail.com>
2024-04-06 15:38:56 -0400

    added primitive classes

--- final-project/DataManager.py
+++ final-project/DataManager.py
@@ -1,5 +1,10 @@
+import random
+
+blm_flt = [0]*64000
+
 class Page():
     def __init__(self, _id) -> None:
+        print(f"\t\t\tMaking page {_id}")
         self.id = _id
         self.content = []
         self.map = {} # each page has a map for getting tuples
@@ -8,21 +13,32 @@ class Page():
 
 class File():
     def __init__(self, name) -> None:
+        print(f"\t\tmaking file: {name}")
         self.attr = name
+        self.map = {}
         self.pages = []
-    def add_page(self) -> None:
+    def update_map(self, id, page_num):
+        self.map[id] = page_num
+
+    def remove_map(self, id):
+        del self.map[id]
+        
+    def add_page(self) -> int:
         _id = len(self.pages)
         self.pages.append(Page(_id))
+        return _id
     def get_page(self, _id) -> Page:
         # do some check bounds etc
         return self.pages[_id]
     
 class Table():
     def __init__(self, name, PK, attrs) -> None:
+        print(f"make table: {name}")
         self.name = name
         self.PK = PK
         self.attrs = {}
         for attr in attrs:
+            print(f"\tMaking attr file: {attr}")
             self.attrs[attr] = File(attr)
 
     def get_file(self, name):
@@ -70,6 +86,11 @@ class ColBuffer():
         # create the record
         # return it to data manager or can send to directly to row buffer
         return
+    
+def LRU(col_cache: ColBuffer):
+    evict = random.randrange(col_cache.max)
+    col_cache.flush(evict)
+    return evict
 
 class RowBuffer():
     def __init__(self, size) -> None:
@@ -106,6 +127,7 @@ class DataManager():
         # does an operation
         # switch case
         # helper for each op
+
         return
     
     def get_txn_ops_from_rl(self):
@@ -135,6 +157,66 @@ class DataManager():
     def insert_to_disc(self):
         raise NotImplementedError
 
-tbl_stb = Table("Starbucks", 'CoffeeID', ['Name', 'Intensity', 'CountryOfOrigin'])
+def update(table, ID, tuple_i):
+    return True
 
+def insert(table, ID, tuple_i):
+
+    # check bloom filter for ID
+    if blm_flt[ID] is 1: # if it is there
+        return update(table, ID, tuple_i) # Call Update
+    else: # if not
+        if tuple_i is None: # if tuple is Null
+            return 0 # no op 
+
+
+    # check catalog for table
+    if not (table in catalog.keys()):
+        # if doesnt exist, Create
+        catalog[table] = Table(table, 'CoffeeID', ['CoffeeName', 'Intensity', 'CountryOfOrigin'])
+    
+    # turn record into tuples
+    # name = tuple_i[0]
+    # intensity = tuple_i[1]
+    # CoI = tuple_i[2]
+    # name_tup = (ID, name)
+    # inten_tup = (ID, intensity)
+    # CoI_tup = (ID, CoI)
+
+    tuples = ( ('CoffeeName', (ID, tuple_i[0])), ('Intensity', (ID, tuple_i[1])), ('CountryOfOrigin', (ID, tuple_i[2])))
+
+    # for each tuple
+    for attr, attr_tup in tuples:
+        # use access method to get to page num for inserting 
+        file = catalog[table].attrs[attr]
+        page_num = file.map[attr_tup[0]]
+        # check page table for page num
+        entry_rec = pg_tbl.get_entry(page_num)
+        if entry_rec is None: # not in the buffer
+            # evict a page (LRU)
+            frame_num = LRU(col_cache)
+            # update page table w that page num and frame num
+            entry = PageTableEntry(frame_num)
+            pg_tbl.set_entry(page_num, entry)
+            # put page in buffer at mapping
+            #page = disc_mngr.get(File, page_num)
+            page = file.get_page(page_num)
+            
+        else:
+
+            page.add_tuple(attr_tup)
+        # Now page is in buffer
+        # use page map to find correct position for tuple in the page
+        # insert the tuple
+        # update the map
+        # update the access method if need be 
+        
+    
+
+    return True
+
+col_cache = ColBuffer(4)
+pg_tbl = PageTable()
+catalog = {}
+catalog['starbucks'] = Table("Starbucks", 'CoffeeID', ['CoffeeName', 'Intensity', 'CountryOfOrigin'])
     
