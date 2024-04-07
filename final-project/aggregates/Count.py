e2bc115dce0eebfe2060f54c53cf14dc72b68cd6
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-07 13:00:13 -0400

    ADD: count aggregate

--- final-project/aggregates/Count.py
+++ final-project/aggregates/Count.py
@@ -1,11 +1,28 @@
 from Common import Aggregate
 
-class ClusteredIndex(Aggregate):
-    def set(self, key, page_key):
-        self.pages[key] = page_key
+class Count(Aggregate):
+    def increment(self, key):
+        count = self.counts.get(key)
+        if count == None:
+            self.counts[key] = 1
+        else:
+            self.counts[key] = count + 1
+
+    def decrement(self, key):
+        count = self.counts.get(key)
+        if count == None:
+            return
+        elif count == 1:
+            del self.counts[key]
+        else:
+            self.counts[key] = count - 1
 
     def get(self, key):
-        return self.pages.get(key)
+        count = self.counts.get(key)
+        if count == None:
+            return 0
+        else:
+            return count
     
     def __init__(self) -> None:
-        self.pages = {}
+        self.counts = {}
