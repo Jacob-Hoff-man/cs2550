562f52790f1b49a0ce993b628d73902b72a62a2f
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-07 10:41:19 -0400

    seperate bash script into 2 (rr, ran)

--- run-ran.sh
+++ run-ran.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-echo "run phase 1 round-robin"
-python3 ./final-project/DbmsSimulator.py final-project/schema.json ran final-project/files/sample1.txt final-project/files/sample2.txt
-
 echo "run phase 1 random"
 python3 ./final-project/DbmsSimulator.py final-project/schema.json rr final-project/files/sample1.txt final-project/files/sample2.txt
---
+++ run-rr.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "run phase 1 round-robin"
+python3 ./final-project/DbmsSimulator.py final-project/schema.json ran final-project/files/sample1.txt final-project/files/sample2.txt
