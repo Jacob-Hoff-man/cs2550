c833aa172b7189218ee0c23d364b9f9a52c08ef8
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-08 18:05:36 -0400

    remove underscores from constants

--- final-project/Constants.py
+++ final-project/Constants.py
@@ -2,13 +2,13 @@ import os
 import time
 
 # Logger Constants
-__LOG_OUTPUT_FILE_EXTENSION__ = 'log'
-__TIME_STRING_FORMAT__ = '%Y%m%d-%H%M%S'
-__TIME_INIT__ = time.strftime(__TIME_STRING_FORMAT__)
+LOG_OUTPUT_FILE_EXTENSION = 'log'
+TIME_STRING_FORMAT = '%Y%m%d-%H%M%S'
+TIME_INIT = time.strftime(TIME_STRING_FORMAT)
 
 # Log Type file paths
-__LOG_OUTPUT_FILE_PATH__ = 'logs/final-project/'
-__LOG_TRXN_MGR_OUTPUT_FILE_PATH__ = os.path.join(__LOG_OUTPUT_FILE_PATH__, 'transaction-manager')
-__LOG_DATA_MGR_OUTPUT_FILE_PATH__ = os.path.join(__LOG_OUTPUT_FILE_PATH__, 'data-manager')
-__LOG_CTLG_MGR_OUTPUT_FILE_PATH__ = os.path.join(__LOG_OUTPUT_FILE_PATH__, 'catalog-manager')
-__LOG_DBMS_SIM_OUTPUT_FILE_PATH__ = os.path.join(__LOG_OUTPUT_FILE_PATH__, 'dbms-simulator')
+LOG_OUTPUT_FILE_PATH = 'logs/final-project/'
+LOG_TRXN_MGR_OUTPUT_FILE_PATH = os.path.join(LOG_OUTPUT_FILE_PATH, 'transaction-manager')
+LOG_DATA_MGR_OUTPUT_FILE_PATH = os.path.join(LOG_OUTPUT_FILE_PATH, 'data-manager')
+LOG_CTLG_MGR_OUTPUT_FILE_PATH = os.path.join(LOG_OUTPUT_FILE_PATH, 'catalog-manager')
+LOG_DBMS_SIM_OUTPUT_FILE_PATH = os.path.join(LOG_OUTPUT_FILE_PATH, 'dbms-simulator')
