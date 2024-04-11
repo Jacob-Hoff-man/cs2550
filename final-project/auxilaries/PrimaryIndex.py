cba7532890f39565a583fb9ddebad64b2d93d990
Jacob Hoffman <cubhoffman@gmail.com>
2024-04-11 18:35:43 -0400

    ADD: PrimaryIndex with no set command

--- final-project/auxilaries/PrimaryIndex.py
+++ final-project/auxilaries/PrimaryIndex.py
@@ -1,35 +1,70 @@
 from Common import Auxiliary, Page
 
 class PrimaryIndex(Auxiliary):
-    def recreate(self, pages):
-        new_pages = {}
-        for page in pages:
-            new_pages[page.content[0]] = page
-        self.pages = new_pages
+    def recreate(self, page_numbers):
+        new_page_numbers = {}
+        for page_number in page_numbers:
+            new_page_numbers[page_numbers] = page_number
+        for overflow in self.overflows:
+            overflow_anchor = overflow[0]
+            overflow_page_number = overflow[1]
+            new_page_numbers[overflow_anchor] = overflow_page_number
+        self.page_numbers = new_page_numbers
         self.overflows = {}
 
     def is_overflow_limit_reached(self):
         return len(self.overflows.items()) == self.overflows
-    
-    def set(self, key, page_key):
-        self.pages[key] = page_key
 
-    def get(self, value):
-        page = self.pages.get(value)
-        if page == None:
-            print('page not found yet')
-            last_index_key = None
-            for index_key in self.pages.keys():
-                if index_key > value:
-                    if last_index_key == None:
+    def get_closest_page(self, value):
+        page_number = self.page_numbers.get(value)
+        if page_number == None:
+            last_anchor = None
+            for anchor in self.page_numbers.keys().sort():
+                if anchor > value:
+                    if last_anchor == None:
                         return None
-                    value_page = self.pages.get(last_index_key)
-                    if value in value_page.content:
-                        return value_page
-                last_index_key = index_key
-            # find key that is 
+                    closest_page = self.page_numbers.get(last_anchor)
+                    overflows = self.overflows.get(last_anchor)
+                    if overflows == None:
+                        return closest_page
+                    else:
+                        last_overflow_page_number = None
+                        for page_numbers in overflows.keys().sort():
+                            overflow_anchor = page_numbers[0]
+                            overflow_page_number = page_numbers[1]
+                            if overflow_anchor > value:
+                                if last_overflow_page_number == None:
+                                    return closest_page
+                                return last_overflow_page_number
+                            elif overflow_anchor == value:
+                                return overflow_page_number
+                            last_overflow_page_number = overflow_page_number
+
+                last_anchor = anchor
+        else:
+            return page_number
+    
+    def get_index(self):
+        return self.page_numbers.keys().sort()
+    
+    # TODO: WIP
+    def set(self, value):
+        value_page = self.get_closest_page(value)
+        if value_page.is_page_limit_reached():
+            if self.is_overflow_limit_reached():
+                return False
+                # recreate  
+            else:
+            # new overflow bucket for (anchor_val, page_num)
+        else:
+        
+    def get(self, value):
+        value_page = self.get_closest_page(value)
+        if value in value_page.content:
+            return value_page
+        return None
     
     def __init__(self) -> None:
-        self.pages= {}
+        self.page_numbers = {}
         self.overflows = {}
         self.overflow_limit = 3
